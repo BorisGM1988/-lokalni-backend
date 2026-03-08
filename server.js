979a8e3dfc60d179a62d8b1287ccee2c64eac5dd
BorisGM1988 <bgutalj1@gmail.com>
2026-03-08 20:26:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 const db = new sqlite3.Database('./users.db');
 

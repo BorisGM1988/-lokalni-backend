392878ad0224e3ef3c88c2c60536a5ee65db9ea9
BorisGM1988 <bgutalj1@gmail.com>
2026-05-23 20:42:45 +0200

    Update nodemailer configuration for Gmail

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ const app = express();
 
 // ===== NODEMAILER SETUP =====
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
